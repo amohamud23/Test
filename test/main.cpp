da39cba847ea4083c79a268e6fbae540daf0c244
Abdi Mohamud <amohamud23@gmail.com>
2018-08-02 10:44:40 -0700

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Abdi Mohamud on 8/2/18.
+//  Copyright © 2018 Abdi Mohamud. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
