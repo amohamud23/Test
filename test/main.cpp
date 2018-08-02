2316ab806c4f5a273afea3568c6fdad450a7a228
Abdi Mohamud <amohamud23@gmail.com>
2018-08-02 14:30:55 -0700

    second commit

--- test/main.cpp
+++ test/main.cpp
@@ -12,5 +12,6 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
     std::cout << "Test Commit\n";
+    std::cout << "Second Commit\n";
     return 0;
 }
