ac6bb33c3a0c8f6a97617e10bdb9c1347b126fbd
Abdi Mohamud <amohamud23@gmail.com>
2018-08-13 19:22:28 -0700

    new files

---
+++ sectarget/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sectarget
+//
+//  Created by Abdi Mohamud on 8/13/18.
+//  Copyright © 2018 Abdi Mohamud. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World Sec Target\n";
+    return 0;
+}
---
+++ test/secondFile.cpp
@@ -0,0 +1,9 @@
+//
+//  secondFile.cpp
+//  test
+//
+//  Created by Abdi Mohamud on 8/13/18.
+//  Copyright © 2018 Abdi Mohamud. All rights reserved.
+//
+
+#include <stdio.h>
